e525e83ff82fb32eca1402cb8a4d2048a710628b
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-29 00:22:39 +0100

    last hope

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-    PROJECT_BASE_URL : "https://hammerhead-app-9r6xm.ondigitalocean.app/online-restaurant-backend/",
+    PROJECT_BASE_URL : "https://hammerhead-app-9r6xm.ondigitalocean.app/api/",
     USER_ROLE : "user",
     ADMIN_ROLE : "admin"
 
