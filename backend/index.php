0484c30d34c3954fa9f86abea672ceed93f2c17d
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-28 16:30:22 +0100

    corrected JWT_SECRET() calls

--- backend/index.php
+++ backend/index.php
@@ -55,7 +55,7 @@
                     Flight::halt(500, "Missing authentication header.");
                 }
 
-                $decoded_token = JWT::decode($token, new Key(Config::JWT_secret(), "HS256"));
+                $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), "HS256"));
 
                 Flight::set("user", $decoded_token->user);
                 Flight::set("jwt_token", $token);
