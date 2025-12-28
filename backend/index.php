1b4d180b06a928188edf03bfbcf66e74d666404e
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-29 00:27:27 +0100

    hopefully

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,21 @@
 <?php 
 
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        exit(0);
+    }
+    
     //why does this path work but this one does not?  require "vendor/autoload.php";
     require __DIR__ . "/vendor/autoload.php";
 
