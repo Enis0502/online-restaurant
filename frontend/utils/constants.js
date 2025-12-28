ad8f8a0091274f1ab6c4fdada9553645a3838926
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-28 16:56:21 +0100

    fix

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-    PROJECT_BASE_URL : "https://hammerhead-app-9r6xm.ondigitalocean.app/",
+    PROJECT_BASE_URL : "https://hammerhead-app-9r6xm.ondigitalocean.app/online-restaurant-backend/",
     USER_ROLE : "user",
     ADMIN_ROLE : "admin"
 
