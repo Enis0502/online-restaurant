1de5fa0c52725fe7c8fb58e0acae13856e4b4974
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-29 02:20:31 +0100

    fix

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,7 @@
 <?php 
-    header("Access-Control-Allow-Origin: https://coral-app-vmx7w.ondigitalocean.app/"); // zameni sa stvarnim frontend URL-om
+    header("Access-Control-Allow-Origin: https://coral-app-vmx7w.ondigitalocean.app"); // zameni sa stvarnim frontend URL-om
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, Authentication");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Access-Control-Allow-Credentials: true");
 
     Flight::route('OPTIONS /*', function(){
