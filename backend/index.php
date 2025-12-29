07dd3cd82c246d87b42d1e2764d6ca1829fad93f
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-29 02:29:41 +0100

    fix

--- backend/index.php
+++ backend/index.php
@@ -4,8 +4,7 @@
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
     header("Access-Control-Allow-Credentials: true");
 
-    // Obradi preflight OPTIONS zahteve
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
         exit();
     }
