0d63b7e409c951d97686225fa69b5f18f6b3d57e
Enis Bulbulusic <enis.bulbulusic@stu.ibu.edu.ba>
2025-12-29 01:43:25 +0100

    fix

--- backend/rest/Database.php
+++ backend/rest/Database.php
@@ -12,15 +12,15 @@ class Database{
         if (self::$connection === null) {
            try {
                self::$connection = new PDO(
-                   "mysql:host=" . Config::DB_HOST() . ";dbname=" . Config::DB_NAME(),
-                   Config::DB_USER(),
-                   Config::DB_PASSWORD(),
-                   [
-
-                       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                   ]
-               );
+            "mysql:host=" . Config::DB_HOST() . ";dbname=" . Config::DB_NAME() . ";port=" . Config::DB_PORT() . ";charset=utf8",
+            Config::DB_USER(),
+            Config::DB_PASSWORD(),
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::MYSQL_ATTR_SSL_CA => __DIR__ . "/certs/ca-certificate.crt" // putanja do CA certifikata
+                ]
+            );
            } catch (PDOException $e) {
                die("Connection failed: " . $e->getMessage());
            }
